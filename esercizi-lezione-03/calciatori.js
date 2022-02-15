a775940bb87de86aa9d7064bc6fd914c9763a48e
Giuseppe Trisciuoglio <giuseppe@adriasonline.it>
2022-02-16 00:02:24 +0100

    feat: aggiunti tutte le soluzioni degli esercizi della seconda lezione

---
+++ esercizi-lezione-03/calciatori.js
@@ -0,0 +1,9 @@
+var calciatori = [
+    {nome: "Lorenzo", cognome: "Pellegrini", maglia: 10 },
+    {nome: "Edin", cognome: "Dzeko", maglia: 23 },
+    {nome: "Federico", cognome: "Chiesa", maglia: 99 },
+    {nome: "Paulo", cognome: "Dybala", maglia: 10 },
+    {nome: "Lorenzo", cognome: "Insigne", maglia: 10 },
+    {nome: "Andrea", cognome: "Belotti", maglia: 9 },
+    {nome: "Antonio", cognome: "Vacca", maglia: undefined },
+];
