c05659953cd1c1faa46446fac9b206396d148a09
Giuseppe Trisciuoglio <giuseppe.trisciuoglio@gmail.com>
2022-02-15 15:31:07 +0100

    feat: aggiunti gli esercizi effettuati nella lezione n° 2

---
+++ esercizi-lezione-02/funzioni/esercizio_1.js
@@ -0,0 +1,34 @@
+/** 
+ * Scrivi una funzione che prenda in input un numero e restituisca TRUE se è un numero primo, FALSE altrimenti.
+ * Scrivi una seconda funzione, che prenda in input un numero N e stampi i primi N numeri primi.
+ * Esempio:
+ *   Input: n = 5
+ *   Output: true 2 3 5 7 11
+ */
+ function isNumeroPrimo(numero){
+    if ( numero == 1 || numero == 2 ) {
+        return true;
+    }
+    for ( var i = 2; i < numero; i++ ) {
+        if ( numero % i == 0 ) {
+            return false;
+        }
+    }
+     return true;
+  }
+  function stampaNumeriPrimi(count = 1){
+      var x = 0;
+      var p = 1;
+      console.log(isNumeroPrimo(count));
+      while(true){
+          if (x === count){
+              break;
+          }
+          if (isNumeroPrimo(p)){
+              console.log(p);
+              x++;
+          }
+          p++;
+      }
+  }
+  stampaNumeriPrimi(20);
