d93928b3662dd6bb724cff209656e8965fece8c3
Giuseppe Trisciuoglio <giuseppe@adriasonline.it>
2022-02-24 19:08:06 +0100

    feat: aggiunta soluzione esercizio da fare a casa

--- esercizi-lezione-04/esercizio-03.js
+++ esercizi-lezione-04/esercizio-03.js
@@ -1,3 +1,60 @@
 /**
- * 
+ * Esercizio del Garage rivisitato con le classi 
  */
+class Automobile {
+    constructor(marca, modello){
+        this.marca = marca;
+        this.modello = modello;
+    }
+
+    get marca(){
+        return this._marca;
+    }
+
+    get modello(){
+        return this._modello;
+    }
+
+    set marca(marca){
+        if (typeof(marca) === "string"){
+            this._marca = marca;
+        }
+    }
+
+    set modello(modello){
+        if (typeof(modello) === "string"){
+            this._modello = modello;
+        }
+    }
+}
+
+class Garage {
+    constructor(){
+        this.database = [];
+    }
+
+    aggiungiAutomobile(automobile){
+        if (automobile instanceof Automobile){
+            this.database.push(automobile);
+        }
+    }
+
+    rimuoviAutomobile(automobile){
+        if (automobile instanceof Automobile){
+            for (var i = 0; i< this.database.length; i++){
+                var el = this.database[i];
+                if (automobile.marca.toLowerCase() === el.marca.toLowerCase()
+                    && automobile.modello.toLowerCase() === el.modello.toLowerCase()){
+                        this.database.splice(i, 1);
+                    }
+            }
+        }
+    }
+
+    stampaAutomobili(){
+        for(var i = 0; i< this.database.length; i++){
+            var auto = this.database[i];
+            console.log(auto.marca + " " + auto.modello);
+        }
+    }
+}
