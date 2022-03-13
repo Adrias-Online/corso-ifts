f861a15b5a5290a341f487a2a21f8274bd0a5142
Giuseppe Trisciuoglio <giuseppe@adriasonline.it>
2022-03-13 20:32:42 +0100

    feat: aggiunti esercizi lezione 08

---
+++ esercizi-lezione-08/array/esercizio01.js
@@ -0,0 +1,12 @@
+/*
+  Invertimi
+  Scrivi una funzione che dato un numero N, generi un array di N numeri casuali
+  e stampi sia l'array ottenuto che quello invertito.
+  Esempio:
+    Input: N = 5
+    Output: array ottenuto = [3, 5, 10, 2, 8], array invertito = [8, 2, 10, 5, 3]
+  Variante:
+    Non utilizzare array/variabili di appoggio per l'inversione.
+  Consigli:
+    Math.random() per generare numeri casuali
+*/
---
+++ esercizi-lezione-08/array/esercizio02.js
@@ -0,0 +1,12 @@
+/***
+ * 
+ * Scrivi una funzione che dato un array di 10 numeri interi ordinati in modo casuale li riordini in modo decrescente.
+  Esempio:
+    Input: array = [3, 7, -2, 5, 8, 1, 2, 5, 6, -4]
+    Output: [8, 7, 6, 5, 5, 3, 2, 1, -4, -2]
+  Variante:
+    Prova ad ordinali in modo crescente.
+  Consigli:
+    Ti consiglio di non usare il metodo sort per questo esercizio ma di provare a scrivere tu il codice 
+    per ordinare gli array, usando if e cicli 
+ */
